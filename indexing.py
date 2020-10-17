6cb41924a45f7ac850cb97f405f49013d02dd559
Nitisha Bharathi <30657775+nitishabharathi@users.noreply.github.com>
2020-10-17 19:55:06 +0530

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -1,53 +1,65 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-import re
-
-
-
+from preprocessing import text_cleaning
 import os
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-import re
-import math
-import pandas as pd
-
-
-
-
-
-def preprocess(file):
-    content = []
-    for line in file:
-        stripped_line = line.strip()
-        stripped_line = re.sub('[^0-9a-zA-Z]+', ' ', stripped_line)
-        line_list=stripped_line.split(' ')
-        for word in line_list:
-            content.append(word)
-    preprocessed_content = []
-    for word in content:
-        word=word.lower()#normalising-lower
-        if word and word not in stop_words:#removing empty strings and stop words
-            word=ps.stem(word)#lemmatize
-            preprocessed_content.append(word)
-    return preprocessed_content
-
 
 
 class BuildIndex:
+    '''
+    Builds Positional Inverted Index.
+    '''
+    
     def __init__(self):
         self.docID_doc = {}
+        '''
+        Dictionary of the form {docID:'filename'}.
+        '''
         self.docID_content = {}
+        '''
+        Dictionary of the form {docID:[term1,term2,...]}.
+        '''
         self.docID = 1
+        '''
+        ID Assigned to a document.
+        '''
         self.vocabulary = set()
+        '''
+        Set of Unique Terms in the Corpus.
+        '''
         self.inverted_pos_index = {}
+        '''
+        Dictionary of the form
+        '''
 
-    def updateVocabulary(self,content):
+    def get_no_of_doc(self):
+        '''
+        Returns no of Documents in the Corpus.
+        '''
+        return self.docID
+
+    def get_inverted_pos_index(self):
+        '''
+        Returns inverted index of the corpus.
+        
+        Output: {term: {docID: [posIndex1, posIndex2]}, ...}, ...} 
+        '''
+        return self.inverted_pos_index
+
+    def get_vocabulary(self):
+        '''
+        Returns set of Unique terms in the Corpus.
+        '''
+        return self.vocabulary
+
+    def update_vocabulary(self, content):
+        '''
+        Updates the Vocabulary Set.
+        '''
         for word in content:
             self.vocabulary.add(word)
 
-    def create_docID_doc(self,doc_ID,doc):
+    def create_docID_doc(self, doc_ID, doc):
+        '''
+        Builds docID-docFileName dictionary.
+        '''
         self.docID_doc[doc_ID] = doc
 
     def create_docID_content(self):
@@ -57,11 +69,10 @@ class BuildIndex:
             file_extension = doc[-3:]
             if file_extension == 'txt':
                 filename = 'data/all/' + doc
-                f = open(filename,'r',encoding="utf-8")
-                #i.docID_doc[i.docID] = doc
-                self.create_docID_doc(i.docID,doc)
-                content = preprocess(f)
-                self.updateVocabulary(content)
+                f = open(filename, 'r', encoding="utf-8")
+                self.create_docID_doc(i.docID, doc)
+                content = text_cleaning(f)
+                self.update_vocabulary(content)
                 self.docID_content[self.docID] = content
                 self.docID += 1
 
@@ -71,10 +82,10 @@ class BuildIndex:
             content = self.docID_content[id]
             fileIndex = {}
             for index, word in enumerate(content):
-                    if word in fileIndex.keys():
-                            fileIndex[word].append(index)
-                    else:
-                            fileIndex[word] = [index]
+                if word in fileIndex.keys():
+                    fileIndex[word].append(index)
+                else:
+                    fileIndex[word] = [index]
             posIndex = fileIndex
             doc_posIndex[id] = posIndex
 
@@ -83,47 +94,17 @@ class BuildIndex:
             for word in doc_posIndex[filename].keys():
                 if word in inverted_pos_index.keys():
                     if filename in inverted_pos_index[word].keys():
-                        inverted_pos_index[word][filename].extend(doc_posIndex[filename][word][:])
+                        inverted_pos_index[word][filename].extend(
+                            doc_posIndex[filename][word][:])
                     else:
                         inverted_pos_index[word][filename] = doc_posIndex[filename][word]
                 else:
-                    inverted_pos_index[word] = {filename: doc_posIndex[filename][word]}
+                    inverted_pos_index[word] = {
+                        filename: doc_posIndex[filename][word]}
         return inverted_pos_index
 
 
 i = BuildIndex()
-stop_words=stopwords.words('english')
-ps = PorterStemmer()    
 
 i.create_docID_content()
 k = i.create_inverted_index()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-    
